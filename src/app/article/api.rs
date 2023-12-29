11005d13ff036ac9ec1c70479fc7111b1b042c26
ASX <mragungsetiaji@gmail.com>
2023-12-29 09:33:13 +0700

    fix: update articles list query

--- src/app/article/api.rs
+++ src/app/article/api.rs
@@ -4,36 +4,80 @@ use super::{ request, response };
 use crate::app::article::tag::model::Tag;
 use crate::app::user::model::User;
 use crate::middleware::auth;
-use crate::schema::users;
+use crate::schema::{ aeticles, tags, users };
 use crate::AppState;
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use diesel::associations::HasTable;
+use serde::Deserialize;
 use uuid::Uuid;
 
 type ArticleIdSlug = Uuid;
 
+#[derive(Deserialize)]
+pub struct ArticlesListQueryParameter {
+    tag: Option<String>,
+    author: Option<String>,
+    favorited: Option<String>,
+    limit: Option<i64>,
+    offset: Option<i64>,
+}
+
 pub async fn index(
     state: web::Data<AppState>,
     req: HttpRequest,
+    params: web::Query<ArticlesListQueryParameter>,
 ) -> impl Responder {
     let auth_user = auth::access_auth_user(&req).expect("invaild user");
     let conn = state
         .pool
         .get()
         .expect("couldn't get db connection from pool");
-    let offset = 0;
-    let limit = 20;
+    
+    /// Calculates the offset and limit for pagination.
+    ///
+    /// The `offset` is the number of items to skip before starting to return items.
+    /// It defaults to 0 if `params.offset` is `None`, but is capped at a maximum of 100.
+    let offset = std::cmp::max(params.offset.to_owned().unwrap_or(0), 100);
+    /// The `limit` is the maximum number of items to return.
+    /// It defaults to 20 if `params.limit` is `None`.
+    let limit = params.limit.unwrap_or(20);
 
     let (articles_list, articles_count) = {
-        use crate::schema::articles::dsl::*;
         use diesel::prelude::*;
 
-        let articles_and_user_list = articles
-            .inner_join(users::table)
-            .offset(offset)
-            .limit(limit)
-            .get_results::<(Article, User)>(&conn)
-            .expect("Error loading articles");
+        let article_and_user_list = {
+            let mut query = articles::table.inner_join(users::table).into_boxed();
+            if let Some(tag_name) &params.tag {
+                let tagged_article_ids = tags::table
+                    .filter(tags::name.eq(tag_name))
+                    .select(tags::articles_id)
+                    .load::<Uuid>(&conn)
+                    .expect("Error loading tagged article ids");
+                query = query.filter(articles::id.eq_any(tagged_article_ids));
+            }
+
+            if let Some(author_name) = &params.author {
+                let article_ids_by_author = users::table
+                    // Note: add username index to users table since we are filtering by username
+                    // here. and we are joining articles table with users table. using inner_join
+                    .inner_join(articles::table)
+                    .filter(users::username.eq(author_name))
+                    .select(users::id)
+                    .load::<Uuid>(&conn)
+                    .expect("Error loading article ids by author");
+                query = query.filter(articles::id.eq_any(article_ids_by_author));
+            }
+
+            if let Some(favorited) = &params.favorited {
+                // TODO: implement
+            }
+
+            query
+                .offset(offset)
+                .limit(limit)
+                .load::<(Article, User)>(&conn)
+                .expect("Error loading articles and users")
+            }
 
         let articles_list = article_and_user_list
             .clone()
@@ -49,10 +93,15 @@ pub async fn index(
             .into_iter()
             .zip(tags_list)
             .collect::<Vec<_>>();
-        let articles_count = articles
-            .select(diesel::dsl::count(id))
-            .first::<i64>(&conn)
-            .expect("Error loading articles count");
+        let articles_count = {
+            use crate::schema::articles;
+            use crate::schema::articles::dsl::*;
+
+            articles
+                .select(diesel::dsl::count(articles::id))
+                .first::<i64>(&conn)
+                .expect("Error loading articles count");
+        };
 
         (articles_list, articles_count)
     };
